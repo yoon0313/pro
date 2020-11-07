74974c623c0a25b7cf8182a4a0ab96abcdf0a5e3
InHo <katatj@naver.com>
2020-11-07 17:03:33 +0900

    ConnetedToDB

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI:'mongodb+srv://devil:a1234@cluster0.u2r1k.mongodb.net/Cryptoberry?retryWrites=true&w=majority'
+    mongoURI:'mongodb+srv://angel:a1234@cluster0.0bdxl.mongodb.net/Cryptoberry?retryWrites=true&w=majority'
 }
